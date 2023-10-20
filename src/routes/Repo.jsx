3ad38b81ba20005129248f85de743a46b17b654b
semilore <semilore@omotade-michaels.com>
2023-10-20 15:38:14 +0100

    add: responsiveness

--- src/routes/Repo.jsx
+++ src/routes/Repo.jsx
@@ -1,5 +1,6 @@
 import getSingleRepo from "../getRepo";
 import { useLoaderData } from "react-router-dom";
+import "../index.css";
 
 export async function repoLoader({ params }) {
 	const repo = await getSingleRepo(params.repoName);
@@ -9,14 +10,27 @@ export async function repoLoader({ params }) {
 export function Repo() {
 	const { repo } = useLoaderData();
 	return (
-		<div className="">
-			<h3>{repo.name}</h3>
-			<h6>Owner: {repo.owner.login}</h6>
+		<div className="repo-data">
+			<h1>{repo.name}</h1>
+			<h3>Owner: {repo.owner.login}</h3>
 			<a href={repo.html_url}>Github link</a>
-			<p>Is repo private? {repo.private ? "True" : "False"}</p>
-			<p>{repo.description}</p>
-			<p>Main Language: {repo.language}</p>
-			<p>Forks count {repo.forks}</p>
+			<div className="row">
+				<p>Is repo private?</p>
+				<div className="data">{repo.private ? "True" : "False"}</div>
+			</div>
+			<div className="row">
+				<p>Description</p>
+				<div className="data">{repo.description}</div>
+			</div>
+			<div className="row">
+				<p>Main Language: </p>
+				<div className="data">{repo.language}</div>
+			</div>
+			<div className="row">
+				<p>
+					Forks count <span>{repo.forks}</span>
+				</p>
+			</div>
 		</div>
 	);
 }
